efff00b46581d1bf6034c96c5e3f824cc605309b
Rosemary Keenan <rosemary.lillian.keenan@gmail.com>
2020-09-09 12:06:01 -0500

    I just realized Joey misspelled Prairie lol

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PrairStem-Team-1
 Capstone Project
+
+# This is a Vue.js application
+See instructions on how to run this app in the README.md located in the helloworld folder.
---
+++ helloworld/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<!--The template is simple html that is inserted into the html-->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/PStem.png">
+    <HelloWorld msg="This is our Capstone"/><!--You can also insert data into a template, like here w/ HellowWorld-->
+    <!--Located in the components folder-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//since we used HelloWorld in the template, we have to import it in the script
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+//we also need to export this part (App.vue) so it can be used in the html
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/*this is just style stuff for the template, this also applies to the components attached to it*/
+</style>
